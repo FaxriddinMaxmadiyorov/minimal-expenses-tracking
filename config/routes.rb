8f6c2d0e344c353caef7b58dfae9d5427fbff4ff
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-10-16 18:53:27 +0500

    views is ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :user, path: '',
+    path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+
   resources :expenses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
