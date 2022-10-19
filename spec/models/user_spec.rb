4edb5fc6536c1d14da18e5abcbcd6cb85d3aa181
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-10-19 21:24:33 +0500

    added tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,71 +8,11 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive.allow_blank }
   end
 
-  let(:valid_attr) { attributes_for(:user) }
-  let(:user) { User.new(valid_attr) }
-
-
-  context 'user email validation' do
-    it 'should not allow incorrect emails #1' do
-      user.email = 'brown'
-      expect(user).to_not be_valid
-    end
-    it 'should not allow incorrect emails #2' do
-      user.email = 'brown@mail'
-      expect(user).to_not be_valid
-    end
-    it 'should not allow incorrect emails #3' do
-      user.email = 'brown@@mail.ru'
-      expect(user).to_not be_valid
-    end
-    it 'should not allow incorrect emails #4' do
-      user.email = '2brown@ma#il.ru'
-      expect(user).to_not be_valid
-    end
-    it 'should allow correct emails #1' do
-      user.email = 'brown@mail.ru'
-      expect(user).to be_valid
-    end
-    it 'should allow correct emails #2' do
-      user.email = '2brown@mail.ru'
-      expect(user).to be_valid
-    end
-    it 'should allow correct emails #3' do
-      user.email = 'brown@2mail.global'
-      expect(user).to be_valid
-    end
-    it 'should allow correct emails #4' do
-      user.email = 'brown@2mail.cc'
-      expect(user).to be_valid
-    end
-  end
-
-  context 'user password validation' do
-    ### Incorrect passwords ###
-    it 'should not allow incorrect passwords #1' do
-      user.password = '12345'
-      expect(user).to_not be_valid
-    end
-    it 'should not allow incorrect passwords #2' do
-      user.password = '1'
-      expect(user).to_not be_valid
-    end
-    it 'should not allow incorrect passwords #3' do
-      user.password = 'asd'
-      expect(user).to_not be_valid
-    end
-    ### Correct passwords ###
-    it 'should allow correct passwords #1' do
-      user.password = '123456'
-      expect(user).to be_valid
-    end
-    it 'should allow correct passwords #2' do
-      user.password = 'QWeasdzxc'
-      expect(user).to be_valid
-    end
-    it 'should allow correct passwords #3' do
-      user.password = '######.#>#>#>#>#'
-      expect(user).to be_valid
+  context 'Associations' do
+    it 'has_many expenses' do
+      association = described_class.reflect_on_association(:expenses)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
     end
   end
 end
